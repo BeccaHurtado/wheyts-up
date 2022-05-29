f0eec0603847b454123839c16df8a22b7f716d6e
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-28 18:56:38 -0700

    to merge

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
-// const { authMiddleware } = require('./utils/auth')
+
 const path = require('path')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
+const {authMiddleware} = require('./utils/auth')
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware
+  context: authMiddleware
 });
 
 const app = express();
